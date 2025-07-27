86994ae035107a5792d3db76019702c297cfb067
WaelGasmi <waelgasmi.dev@gmail.com>
2025-07-27 15:53:41 +0100

    fixed auth sign up ui

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -56,11 +56,11 @@ export const signup = async (req: Request, res: Response) => {
     generateTokenAndSetCookie(res, newUser._id.toString());
 
     return res.status(200).json({
-      message: "Signing up success",
+      message: "success",
       user: { firstName, lastName, email },
     });
   } catch (error) {
-    return res.status(500).json({ message: "success" });
+    return res.status(500).json({ message: "Error" });
   }
 };
 
